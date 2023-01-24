b8479c2aef467fbf945b50b68107df3e4d5b968f
ktiana <108888662+ktiana@users.noreply.github.com>
2023-01-24 18:06:59 +0000

    Deploying to gh-pages from @ ktiana/goit-react-hw-05-movies-nodepl@f9cfb0856288808facad6ff63099cfc976c17f87 🚀

---
+++ static/js/216.57b906ec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[216],{216:function(e,n,t){t.r(n);var i=t(184);n.default=function(){return(0,i.jsx)("div",{children:(0,i.jsx)("p",{children:"404 not found"})})}}}]);
+//# sourceMappingURL=216.57b906ec.chunk.js.map
